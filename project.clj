cb620da0b6597e92b08e0af3199d5e8b091cfbbd
John Newman <john.michael.newman@gmail.com>
2018-08-31 21:09:52 -0400

    testing without incanter

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                [incanter "1.5.5"]
+                ;[incanter "1.5.5"] testing without incanter
                 [com.medallia.word2vec/Word2VecJava "0.9.0"]
                 ])
